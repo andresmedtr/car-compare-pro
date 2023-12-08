fa70dbbfd9562937d5fda7ec7f8af42b018a307f
Andres Medina <123752360+andresmedtr@users.noreply.github.com>
2023-12-08 21:54:13 +0000

    added env

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,23 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
         publicPath: '/'
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            name: 'vendor',
+        },
+        minimizer: [
+            new TerserPlugin(),
+            new OptimizeCSSAssetsPlugin()
+        ],
     },
     plugins: [
         new Dotenv({
