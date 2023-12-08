8b90b3eea7debce40bac1aa80feaeb19dadc75a8
Andres Medina <123752360+andresmedtr@users.noreply.github.com>
2023-12-08 22:36:09 +0000

    added a new head

---
+++ migrations/versions/8459755e5025_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8459755e5025
+Revises: ac6ed40a6a95
+Create Date: 2023-12-08 22:35:49.721077
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8459755e5025'
+down_revision = 'ac6ed40a6a95'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
